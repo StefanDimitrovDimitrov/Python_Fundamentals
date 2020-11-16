397eed11fc4dc0132c57a671e016c4f915aebca0
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2020-11-16 20:34:40 +0200

    Added another solution

--- PycharmProject/Python Code Challenges/Find Prime Numbers.py
+++ PycharmProject/Python Code Challenges/Find Prime Numbers.py
@@ -1,23 +1,38 @@
-import math
-def primeFactors(n):
+# import math
+# def primeFactors(n):
+#
+#     while n % 2 == 0:
+#         result.append(2),
+#         n = n / 2
+#
+#     for i in range(3, int(math.sqrt(n))+1,2):
+#
+#         while n % i ==0:
+#             result.append(i)
+#             # print(i),
+#             n = n / i
+#
+#     if n > 2:
+#         print(n)
+#
+# result = []
+#
+# n = int(input())
+# primeFactors(n)
+#
+# print(result)
 
-    while n % 2 == 0:
-        result.append(2),
-        n = n / 2
+def get_prime_factors(N):
+    factors = []
+    divisor = 2
+    while(divisor <= N):
+        if (N % divisor) == 0:
+            factors.append(divisor)
+            N = N/divisor
+        else:
+            divisor += 1
+    return factors
 
-    for i in range(3, int(math.sqrt(n))+1,2):
-
-        while n % i ==0:
-            result.append(i)
-            # print(i),
-            n = n / i
-
-    if n > 2:
-        print(n)
-
-result = []
 
 n = int(input())
-primeFactors(n)
-
-print(result)
+print(get_prime_factors(n))
